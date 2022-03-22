4899452830b151b3de384e19fda671ae8c452d96
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2022-03-22 14:25:15 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$number= $_POST['phone'];
+$message= $_POST['message'];
+$to = "chagantivenkataramireddy1@mail.com";
+$subject = $_POST['subject'];
+$txt ="Name : ". $name . "\r\n  Email : " . $email . "\r\n  Phone Number : " . $number . "\r\n Message :" . $message;
+$headers = "From: ". $email;
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("index.html");
+?>
